4f3790c343fcf7580ebcaf43ffce101804e28993
Lauren Lavoie <llavoie@grameenfoundation.org>
2016-02-05 12:19:03 -0800

    add query for states where Kilkari deployed

---
+++ kilkari/states-where-kilkari-deployed.sql
@@ -0,0 +1,6 @@
+## States where Kilkari is deployed ##
+
+SELECT * FROM motech_data_services.nms_deployed_services as dep
+left join nms_states as st
+on dep.state_id_OID = st.id
+where service = 'KILKARI';
