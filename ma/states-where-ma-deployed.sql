56c4fb006d1a33508c2c5332c0c21b4c4f75d268
Lauren Lavoie <llavoie@grameenfoundation.org>
2016-02-05 12:17:33 -0800

    add missing semicolon

--- ma/states-where-ma-deployed.sql
+++ ma/states-where-ma-deployed.sql
@@ -3,4 +3,4 @@
 SELECT * FROM motech_data_services.nms_deployed_services as dep 
 left join nms_states as st
 on dep.state_id_OID = st.id
-where service = 'MOBILE_ACADEMY'
+where service = 'MOBILE_ACADEMY';
